784604cfa3738b6414da6289ee7836b93a51abe5
GUSTAVO DE AZEVEDO SOARES <51483265+g81marketing@users.noreply.github.com>
2021-07-22 18:57:38 -0300

    Add files via upload

---
+++ PROJETO URNA/JAVASCRIPT/etapas.js
@@ -0,0 +1,52 @@
+
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'prefeito.jpeg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'vice_prefeito.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'prefeito.jpeg', legenda: 'Prefeito'},
+                    {url:'vice_prefeito.jpeg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'prefeito.jpeg', legenda: 'Prefeito'},
+                    {url: 'vice_prefeito.jpeg', legenda: 'Vice-Prefeito', small: true},
+                ]
+            },
+        ]
+    }
+];
